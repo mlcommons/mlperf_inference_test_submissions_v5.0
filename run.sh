50790b1720f905e03deeca62fffe9ca35c6810a1
Arjun Suresh <arjunsuresh1987@gmail.com>
2024-10-10 18:37:40 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,4 +6,5 @@ if [ ! -e docs ]; then
 fi
 
 cp docs/docinit.sh .
+
 INFERENCE_RESULTS_VERSION=v4.1 INFERENCE_RESULTS_REPO_OWNER=${INFERENCE_RESULTS_REPO_OWNER} INFERENCE_RESULTS_REPO_BRANCH=main INFERENCE_RESULTS_REPO_NAME=mlperf_inference_test_submissions_v5.0 bash docinit.sh
