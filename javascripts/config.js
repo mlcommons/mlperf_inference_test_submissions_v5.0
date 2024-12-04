33b1cb38d2bab715fd2bd62f3f0f664fe459cd21
nathanw-mlc <nathanw-mlc@users.noreply.github.com>
2024-12-04 18:21:49 +0000

    Deployed f03e401 with MkDocs version: 1.6.1

---
+++ javascripts/config.js
@@ -0,0 +1,7 @@
+const results_version="v4.1";
+var repo_owner="mlcommons";
+var repo_branch="refs/heads/main";
+var repo_name="mlperf_inference_test_submissions_v5.0";
+const dbVersion ="266";
+const default_category ="edge";
+const default_division ="open";
